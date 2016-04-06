55e8f598df450e98eaa934e629253e1a342410d9
zhenglipeng <1252012805@qq.com>
2016-04-06 18:40:17 +0800

    [bug fix] api链接调整

--- app/app.js
+++ app/app.js
@@ -37,9 +37,9 @@ define([
 
     myApp.constant('GLOBAL', {
         size: 10,
-        host: 'https://54.222.199.235:8443/oapi/v1',
+        host: '/oapi/v1',
         host_wss: 'wss://54.222.199.235:8443/oapi/v1',
-        login_uri: 'http://localhost:9090/login'
+        login_uri: '/login'
     })
     .constant('AUTH_EVENTS', {
         loginNeeded: 'auth-login-needed',
