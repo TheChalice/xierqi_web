903beabf41a909c3e802dda3d4787c3d4e7c4a70
zhenglipeng <1252012805@qq.com>
2016-04-06 18:32:10 +0800

    [bug fix] 创建构建页最大构建时间选择限制，点击时不允许超过范围，但是手动填写仍然可以超范围，不过会有提示

--- app/app.js
+++ app/app.js
@@ -37,9 +37,9 @@ define([
 
     myApp.constant('GLOBAL', {
         size: 10,
-        host: '/oapi/v1',
+        host: 'https://54.222.199.235:8443/oapi/v1',
         host_wss: 'wss://54.222.199.235:8443/oapi/v1',
-        login_uri: '/login'
+        login_uri: 'http://localhost:9090/login'
     })
     .constant('AUTH_EVENTS', {
         loginNeeded: 'auth-login-needed',
