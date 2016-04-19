399fcc5dbac67c5a13f29ee61c527303234ae0b1
jingxy <953885034@qq.com>
2016-04-19 17:24:34 +0800

    new feature 服务部署高级配置

--- app/views/service_create/service_create.js
+++ app/views/service_create/service_create.js
@@ -7,32 +7,45 @@ angular.module('console.service.create', [
             ]
         }
     ])
-    .controller('ServiceCreateCtrl', [ '$rootScope', '$scope', '$log', 'ImageStream', 'DeploymentConfig', 'ImageSelect',
-        function ($rootScope, $scope, $log, ImageStream, DeploymentConfig, ImageSelect) {
+    .controller('ServiceCreateCtrl', [ '$rootScope', '$scope', '$log', 'ImageStream', 'DeploymentConfig', 'ImageSelect','BackingServiceInstance',
+        function ($rootScope, $scope, $log, ImageStream, DeploymentConfig, ImageSelect,BackingServiceInstance) {
         $log.info('ServiceCreate');
         $scope.deploymentConfig = {
             metadata: {
                 name: ''
             },
-            spec: {
-               containers: {
-                   name: '',
-                   image: '',
-                   ports: {
-                       containerPort: '',
-                       protocol: ''
-                   },
-                   VolumeMounts: {
+            template: {
+                spec: {
+                    containers: [{
+                        name: '',
+                        image: '',
+                        ports: {
+                            containerPort: '',
+                            protocol: ''
+                        },
 
-                   }
-               },
-                Volumes: {
-                    name:'',
-                    secret:'',
-                    secretName:''
+                        VolumeMounts: {}
+                    }],
+                    replicas: 5,
+                    restartPolicy: "Always",
+                    Volumes: {
+                        name: '',
+                        secret: '',
+                        secretName: ''
+                    }
                 }
             }
         };
+        $scope.envList = [
+                {
+                    name:"DATAFOUNDRY_APISERVER_ADDR",
+                    value:"lab-test.dataos.io:8443"
+                },
+                {
+                    name:"DATAFOUNDRY_APISERVER_ADDR",
+                    value:"lab-test.dataos.io:8443"
+                }
+            ];
         $log.info("ImageStream");
         $scope.loadImageStream = function() {
             ImageSelect.open();
@@ -41,6 +54,23 @@ angular.module('console.service.create', [
         $scope.createDC = function() {
             console.log("deploymentConfig", $scope.deploymentConfig);
        };
+       var bsiList = function(){
+           BackingServiceInstance.get({namespace: $rootScope.namespace}, function (data) {
+               $log.info("bsiList", data);
+               $scope.BsiList = data.items;
+
+           });
+       }
+       $scope.addEnv = function(){
+           var newenv = {};
+           $scope.envList.push(newenv);
+
+
+       };
+       $scope.delEnv = function(idx){
+           $scope.envList.splice(idx,1);
+       }
+       bsiList ();
        $scope.createDC();
     }])
 
