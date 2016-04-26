cdbac3e7c5bb65f0ffd59f4e9724987cbad7eb92
jingxy <953885034@qq.com>
2016-04-26 18:11:06 +0800

    new feature 后端服务列表

--- app/views/backing_service/backing_service.js
+++ app/views/backing_service/backing_service.js
@@ -6,6 +6,25 @@ angular.module('console.backing_service',[
         ]
     }
 ])
-.controller('BackingServiceCtrl',['$log', function ($log){
-
+.controller('BackingServiceCtrl',['$log', '$scope','BackingService',function ($log,$scope,BackingService){
+    var loadBs = function(){
+        BackingService.get({namespace:'openshift'},function(data){
+            $log.info('loadBs',data);
+            $scope.items = data.items;
+        })
+    }
+    loadBs();
+    $scope.search = function (key, txt) {
+        $scope.items = [];
+        txt = txt.replace(/\//g, '\\/');
+        //txt = txt.replace(/\./g, '\\.');
+        var reg = eval('/' + txt + '/');
+        angular.forEach($scope.items, function(item) {
+            if (key == 'all' || key == 'metadata.name') {
+                if (reg.test(item.metadata.name)) {
+                    $scope.items.push(item);
+                }
+            }
+        })
+    };
 }]);
