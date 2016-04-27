f850b09b362de56be32e35d8c25371d97ac1caff
zhenglipeng <1252012805@qq.com>
2016-04-27 10:16:40 +0800

    [bug fix]: 跳转到后端服务详情页方式调整

--- app/views/backing_service/backing_service.js
+++ app/views/backing_service/backing_service.js
@@ -18,10 +18,6 @@ angular.module('console.backing_service',[
         })
     }
     loadBs();
-    $scope.bsdetails = function(){
-        $state.go('backing_service_detail');
-    }
-
     $scope.search = function (txt) {
         if(!txt){
             $scope.items = $scope.data;
