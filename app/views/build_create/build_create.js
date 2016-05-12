6ea3edec4fb2f2f61d32cc7eed041b9b344619ff
zhenglipeng <1252012805@qq.com>
2016-05-12 12:57:53 +0800

    [bug fix] 创建构建提交数据时给出明确提示，并且禁用提交按钮

--- app/views/build_create/build_create.js
+++ app/views/build_create/build_create.js
@@ -37,6 +37,7 @@ angular.module('console.build.create', [])
         $scope.completionDeadlineMinutes = 30;
 
         $scope.create = function() {
+            $scope.creating = true;
             var imageStream = {
                 metadata: {
                     name: $scope.buildConfig.metadata.name
@@ -56,6 +57,7 @@ angular.module('console.build.create', [])
                     createBuildConfig($scope.buildConfig.metadata.name);
                 } else {
                     Alert.open('错误', res.data.message, true);
+                    $scope.creating = false;
                 }
             });
         };
@@ -67,7 +69,9 @@ angular.module('console.build.create', [])
             BuildConfig.create({namespace: $rootScope.namespace}, $scope.buildConfig, function(res){
                 $log.info("buildConfig", res);
                 createBuild(res.metadata.name);
+                $scope.creating = false;
             }, function(res){
+                $scope.creating = false;
                 if (res.data.code == 409) {
                     Alert.open('错误', "构建名称重复", true);
                 } else {
