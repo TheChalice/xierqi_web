2d79b2cb5681a9a7bb46ede620e5af06a388456c
jingxy <953885034@qq.com>
2016-07-08 17:10:16 +0800

    首页调整

--- app/views/home/index/index.js
+++ app/views/home/index/index.js
@@ -3,6 +3,8 @@
 angular.module('home.index', [])
     .controller('IndexCtrl', ['$scope', '$log', function ($scope, $log) {
         $log.info('Index');
+        var h=document.documentElement.clientHeight
+        $('.banner').height(h *0.7);
 
     }]);
 
