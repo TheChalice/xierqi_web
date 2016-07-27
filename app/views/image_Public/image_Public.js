f73f532348a33eed15d27c71d3c7ffc2b2ff8761
sorcerer <jt5016224@163.com>
2016-07-27 18:26:10 +0800

    image修改

--- app/views/image_Public/image_Public.js
+++ app/views/image_Public/image_Public.js
@@ -8,8 +8,34 @@ angular.module('console.image_Public', [
             ]
         }
     ])
-    .controller('imagePublicCtrl', ['$scope',
-        function ($scope){
+    .controller('imagePublicCtrl', ['$http','$state','$scope',
+        function ($http,$state,$scope){
+            //console.log($state.params.name);
+            $scope.name=$state.params.name;
+            $scope.items=[]
+            $http.get('/registry/api/repositories/tags', {params: {repo_name:$state.params.name}})
+                .success(function (tags) {
+                    //console.log(tags);
+                    angular.forEach(tags, function (tag,i) {
+                        $http.get('/registry/api/repositories/manifests',
+                            {params: {repo_name:$scope.name,tag:tag}})
+                            .success(function (tagmessage) {
 
+                                $scope.items.push({tag:tag,tagcen:tagmessage});
+                                if ($scope.items.length == tags.length) {
+                                    //console.log($scope.items);
+
+                                    angular.forEach($scope.items, function (item,i) {
+                                        $scope.items[i].sorttime=(new Date(item.tagcen.Created)).getTime()
+                                    })
+                                    //console.log($scope.items);
+                                    $scope.items.sort(function (x, y) {
+                                        return x.sorttime > y.sorttime ? -1 : 1;
+                                    });
+                                    //console.log($scope.items);
+                                }
+                            })
+                    })
+                })
         }])
 
