7cd8f5cf04eb1ec4e4235b461a193cf69967b86f
jingxy <953885034@qq.com>
2016-08-01 16:02:12 +0800

    资源管理，添加密钥

--- app/views/create_secret/create_secret.js
+++ app/views/create_secret/create_secret.js
@@ -6,7 +6,8 @@ angular.module('console.create_secret', [
     }
 ]).controller('createSecretCtrl',['$scope','$rootScope','secretskey','$base64',function($scope,$rootScope,secretskey,$base64){
         $scope.grid = {
-            secreteno : false
+            secreteno : false,
+            secretnames : true
         }
     $scope.secrets = {
         "kind": "Secret",
@@ -26,8 +27,30 @@ angular.module('console.create_secret', [
     }
     $scope.rmsecret = function(idx){
         $scope.secretsarr.splice(idx,1);
+        if($scope.secretsarr.length<=0){
+            $scope.grid.secreteno = false;
+        }
     }
+    $scope.checknames = function(){
+        var r = /^[a-z][-a-z0-9]*[a-z0-9]$/;
+        if(!r.test($scope.secrets.metadata.name)){
+            $scope.grid.secretnames = false;
+        }else{
+            $scope.grid.secretnames = true;
+        }
 
+        console.log($scope.grid.secretnames)
+    }
+    $scope.checkedkv = function(){
+        var r = /^\.?[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$/; // key值的验证;
+       for(var i = 0; i < $scope.secretsarr.length; i++ ){
+           if($scope.secretsarr[i].idx1 && $scope.secretsarr[i].idx2 && r.test($scope.secretsarr[i].idx1)){
+               $scope.grid.secreteno = true;
+           }else{
+               $scope.grid.secreteno = false;
+           }
+       }
+    }
 
     $scope.postsecret = function(){
         console.log($scope.secretsarr)
@@ -35,11 +58,13 @@ angular.module('console.create_secret', [
             for(var j=i+1;j<$scope.secretsarr.length;j++){
                 if($scope.secretsarr[i].idx1 == $scope.secretsarr[j].idx1){
                     console.log('key值重了!!!');
+                    $scope.grid.secreteno = false;
                     return;
                 }
             }
             if(!$scope.secretsarr[i].idx1 || !$scope.secretsarr[i].idx2){
-                console.log('err!!!!!!!!!!!!')
+                console.log('err!!!!!!!!!!!!');
+                $scope.grid.secreteno = false;
                 return;
             }
             var k = $scope.secretsarr[i].idx1;
@@ -47,6 +72,7 @@ angular.module('console.create_secret', [
             $scope.secrets.data[k] = $base64.encode(v);
             console.log($scope.secrets.data)
         }
+        $scope.grid.secreteno = true;
         secretskey.create({namespace: $rootScope.namespace},$scope.secrets , function (res) {
             console.log('createconfig----',res);
         })
