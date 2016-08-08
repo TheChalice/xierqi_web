d1603e5b4b43612cbd887534632001a27999e101
sorcerer <jt5016224@163.com>
2016-08-08 16:52:43 +0800

    持久化卷

--- app/views/constantly_detail/constantly_detail.js
+++ app/views/constantly_detail/constantly_detail.js
@@ -16,7 +16,7 @@ angular.module('console.constantly_detail', [
                 DeploymentConfig.get({namespace: $rootScope.namespace}, function (dcres) {
                     angular.forEach(dcres.items, function (dcitem,i) {
                         angular.forEach(dcitem.spec.template.spec.volumes, function (item,i) {
-                            if (res.metadata.name == item.persistentVolumeClaim.claimName) {
+                            if (item.persistentVolumeClaim&&res.metadata.name == item.persistentVolumeClaim.claimName) {
                                 res.arr.push(dcitem.metadata.name)
                             }
                         })
@@ -29,13 +29,18 @@ angular.module('console.constantly_detail', [
             })
             $scope.delete= function () {
                 Confirm.open("删除持久化卷", "您确定要删除持久化卷吗？", "持久化卷中的数据将被删除", "stop").then(function(){
+                    if ($scope.persistents.arr.length > 0) {
+                        Confirm.open("删除持久化卷", "删除持久化卷失败", "持久化卷已经挂载在容器中,您需要先停止服务,         卸载持久化卷后,才能删除.", null,true)
 
+                    }else {
                         delvolume.del({namespace: $rootScope.namespace,name:$stateParams.name}, function (res) {
                             console.log(res);
                             $state.go('console.resource_management', {index: 1})
                         }, function (err) {
-                            Confirm.open("删除持久化卷", "删除持久化卷失败", "持久化卷已经挂载在容器中,您需要先停止服务,         卸载持久化卷后,才能删除.", null,true)
+
                         })
+                    }
+
 
 
                 })
