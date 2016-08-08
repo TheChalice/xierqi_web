9b9a1163aaed836e11ef8cfb12f8c24ae8db75cc
sorcerer <jt5016224@163.com>
2016-08-08 11:25:26 +0800

    webhookapi更改

--- app/views/resource_management/resource_management.js
+++ app/views/resource_management/resource_management.js
@@ -5,8 +5,8 @@ angular.module('console.resource_management', [
             'components/searchbar/searchbar.js',
         ]
     }
-]).controller('resmanageCtrl',['DeploymentConfig','persistent','$state','$rootScope','$scope','configmaps','secretskey',
-    function(DeploymentConfig,persistent,$state,$rootScope,$scope,configmaps,secretskey){
+]).controller('resmanageCtrl',['$log','Ws','DeploymentConfig','persistent','$state','$rootScope','$scope','configmaps','secretskey',
+    function($log,Ws,DeploymentConfig,persistent,$state,$rootScope,$scope,configmaps,secretskey){
     $scope.grid = {
         page: 1,
         size: 10,
@@ -24,38 +24,99 @@ angular.module('console.resource_management', [
     } else {
         $scope.check = false
     }
-        persistent.get({namespace: $rootScope.namespace}, function (res) {
+        function persistentlist(){
+            persistent.get({namespace: $rootScope.namespace}, function (res) {
 
-            DeploymentConfig.get({namespace: $rootScope.namespace}, function (resdc) {
-                //console.log('dc',resdc);
-                angular.forEach(res.items, function (volitem,i) {
-                    res.items[i].arr=[]
-                    angular.forEach(resdc.items, function (dcitem,k) {
-                        angular.forEach(dcitem.spec.template.spec.volumes, function (dcvolitem,j) {
-                            if (volitem.metadata.name == dcvolitem.persistentVolumeClaim.claimName) {
-                                res.items[i].arr.push(dcitem.metadata.name)
-                            }
+                DeploymentConfig.get({namespace: $rootScope.namespace}, function (resdc) {
+                    //console.log('dc',resdc);
+                    angular.forEach(res.items, function (volitem,i) {
+                        res.items[i].arr=[]
+                        angular.forEach(resdc.items, function (dcitem,k) {
+                            angular.forEach(dcitem.spec.template.spec.volumes, function (dcvolitem,j) {
+                                if (volitem.metadata.name == dcvolitem.persistentVolumeClaim.claimName) {
+                                    res.items[i].arr.push(dcitem.metadata.name)
+                                }
+                            })
+                            //volitem.metadata.name==dcitem.spec.template.spec.volumes
                         })
-                        //volitem.metadata.name==dcitem.spec.template.spec.volumes
-                    })
+                    });
+
+                    if (res.items&&res.items.length > 0) {
+                        angular.forEach(res.items, function (item,i) {
+                            res.items[i].sorttime=(new Date(item.metadata.creationTimestamp)).getTime()
+                        })
+                        //console.log($scope.items);
+
+                        res.items.sort(function (x, y) {
+                            return x.sorttime > y.sorttime ? -1 : 1;
+                        });
+                        $scope.resourceVersion = res.metadata.resourceVersion;
+                        watchPc(res.metadata.resourceVersion);
+                        $scope.persistents=res;
+                        console.log('chijiu',res);
+                    }
+
                 })
-                angular.forEach(res.items, function (item,i) {
-                    res.items[i].sorttime=(new Date(item.metadata.creationTimestamp)).getTime()
-                })
-                //console.log($scope.items);
-                res.items.sort(function (x, y) {
-                    return x.sorttime > y.sorttime ? -1 : 1;
+
+
+            }, function (err) {
+
+            });
+        }
+
+        var watchPc = function(resourceVersion){
+            Ws.watch({
+                api: 'k8s',
+                resourceVersion: resourceVersion,
+                namespace: $rootScope.namespace,
+                type: 'persistentvolumeclaims',
+                name: ''
+            }, function(res){
+                var data = JSON.parse(res.data);
+                updatePC(data);
+                console.log(data);
+            }, function(){
+                $log.info("webSocket start");
+            }, function(){
+                $log.info("webSocket stop");
+                var key = Ws.key($rootScope.namespace, 'persistentvolumeclaims', '');
+                if (!$rootScope.watches[key] || $rootScope.watches[key].shouldClose) {
+                    return;
+                }
+                watchPc($scope.resourceVersion);
+            });
+        };
+        var updatePC = function(data){
+            if (data.type == 'ERROR') {
+                $log.info("err", data.object.message);
+                Ws.clear();
+                persistentlist();
+                return;
+            }
+            // if (data.type == 'DELETE') {
+            //
+            // }
+
+            $scope.resourceVersion = data.object.metadata.resourceVersion;
+
+            if (data.type == 'ADDED') {
+                $scope.rcs.items.push(data.object);
+            }else if (data.type == "MODIFIED") {
+                angular.forEach($scope.items, function(item, i){
+                    if (item.rc.metadata.name == data.object.metadata.name) {
+                        $scope.items[i].rc = data.object;
+                        console.log('updatedata========',data);
+                        console.log('$scope.items[i]-----------',$scope.items[i]);
+                        //isNormal($scope.items);
+                        $scope.$apply();
+                    }
                 });
-                $scope.persistents=res;
-                //console.log('chijiu',res);
-            })
 
 
-        }, function (err) {
 
-        })
-
-  //////////////////  配置卷
+            }
+        }
+  ////////////////  配置卷
     $scope.$watch('grid.page', function(newVal, oldVal){
         if (newVal != oldVal) {
             refresh(newVal);
@@ -71,22 +132,25 @@ angular.module('console.resource_management', [
     $scope.loadconfigmaps = function(){
         configmaps.get({namespace: $rootScope.namespace},function(res){
             console.log(res);
-            angular.forEach(res.items, function (item,i) {
-                res.items[i].sorttime=(new Date(item.metadata.creationTimestamp)).getTime()
-            })
-            //console.log($scope.items);
-            res.items.sort(function (x, y) {
-                return x.sorttime > y.sorttime ? -1 : 1;
-            });
-            if(!res.items) {
-                $scope.configitems = [];
-            }else{
-                $scope.configitems = res.items;
-            }
+            if (res.items&&res.items.length > 0) {
+                angular.forEach(res.items, function (item,i) {
+                    res.items[i].sorttime=(new Date(item.metadata.creationTimestamp)).getTime()
+                })
+                //console.log($scope.items);
+                res.items.sort(function (x, y) {
+                    return x.sorttime > y.sorttime ? -1 : 1;
+                });
+                if(!res.items) {
+                    $scope.configitems = [];
+                }else{
+                    $scope.configitems = res.items;
+                }
                 $scope.grid.total = $scope.configitems.length;
                 $scope.grid.page = 1;
                 $scope.grid.txt = '';
                 refresh(1);
+            }
+
 
         })
 
@@ -118,23 +182,26 @@ angular.module('console.resource_management', [
     $scope.loadsecrets = function(){
         secretskey.get({namespace:$rootScope.namespace},function(res){
             console.log('-------loadsecrets',res);
-            angular.forEach(res.items, function (item,i) {
-                res.items[i].sorttime=(new Date(item.metadata.creationTimestamp)).getTime()
-            })
-            console.log(res.items);
-            //console.log($scope.items);
-            res.items.sort(function (x, y) {
-                return x.sorttime > y.sorttime ? -1 : 1;
-            });
-            if(res.items){
-                $scope.secretitems = res.items;
-            }else {
-                $scope.secretitems = []
+            if (res.items&&res.items.length > 0) {
+                angular.forEach(res.items, function (item,i) {
+                    res.items[i].sorttime=(new Date(item.metadata.creationTimestamp)).getTime()
+                })
+                console.log(res.items);
+                //console.log($scope.items);
+                res.items.sort(function (x, y) {
+                    return x.sorttime > y.sorttime ? -1 : 1;
+                });
+                if(res.items){
+                    $scope.secretitems = res.items;
+                }else {
+                    $scope.secretitems = []
+                }
+                $scope.secrets.total = $scope.secretitems.length;
+                $scope.secrets.page = 1;
+                $scope.secrets.txt = '';
+                secretrefresh(1);
             }
-            $scope.secrets.total = $scope.secretitems.length;
-            $scope.secrets.page = 1;
-            $scope.secrets.txt = '';
-            secretrefresh(1);
+
         })
     }
 
