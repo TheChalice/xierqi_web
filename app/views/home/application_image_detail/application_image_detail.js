9e4b8caca895f7a4cc740fb3967691dea6bce08a
jingxy <953885034@qq.com>
2016-08-31 16:10:37 +0800

    应用中心页面

---
+++ app/views/home/application_image_detail/application_image_detail.js
@@ -0,0 +1,16 @@
+/**
+ * Created by jxy on 16/8/31.
+ */
+/**
+ * Created by jxy on 16/8/30.
+ */
+angular.module('home.application_image_detail', [
+        {
+            files: [
+                'views/home/application_image_detail/application_image_detail.css'
+            ]
+        }
+    ])
+    .controller('application_image_detailCtrl', ['$scope', '$log', function ($scope, $log) {
+
+    }]);
