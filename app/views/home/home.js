33b004c2b350ec85b8427d3851baea5be38c33db
fooles <18730471450@163.com>
2016-09-30 15:46:49 +0800

    Merge branch 'develop' of https://github.com/asiainfoLDP/datafoundry_web into feature/jfeature
    # Conflicts:
    #	app/views/home/introduce/introduce.js

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -25,8 +25,7 @@ angular.module('home', [])
             //路由监听事件
             //console.log($state.current.name);
 
-            switch($state.current.name)
-            {
+            switch ($state.current.name) {
                 case 'home.index':
                     $scope.whereclick = '首页'
                     break;
@@ -42,11 +41,18 @@ angular.module('home', [])
                 default:
                     $scope.whereclick = '首页'
             }
+            var images = new Array()
+
+            function preload() {
+                for (var i = 0; i < arguments.length; i++) {
+                    images[i] = new Image()
+                    images[i].src = arguments[i]
+                }
+            };
             $rootScope.$on('$stateChangeStart',
                 function (event, toState, toParams, fromState, fromParams) {
                     console.log(toState.name);
-                    switch(toState.name)
-                    {
+                    switch (toState.name) {
                         case 'home.index':
                             $scope.whereclick = '首页'
                             break;
@@ -63,10 +69,47 @@ angular.module('home', [])
                             $scope.whereclick = '首页'
                     }
                     if (toState.name !== "home.introduce") {
-                        $('html').css('overflow','auto');
-                        window.onmousewheel = document.onmousewheel=true;
-                    }else {
-                        $('html').css('overflow','hidden');
+                        $('html').css('overflow', 'auto');
+                        window.onmousewheel = document.onmousewheel = true;
+                    } else {
+                        $('html').css('overflow', 'hidden');
+
+
+                        preload(
+                            "views/home/introduce/img/DF-111.png",
+                            "views/home/introduce/img/DF-15.png",
+                            "views/home/introduce/img/DF-16.png",
+                            "views/home/introduce/img/DF-17.png",
+                            "views/home/introduce/img/DF-19.png",
+                            "views/home/introduce/img/DF-20.png",
+                            "views/home/introduce/img/DF-21.png",
+                            "views/home/introduce/img/DF-22.png",
+                            "views/home/introduce/img/DF-24.png",
+                            "views/home/introduce/img/DF-25.png",
+                            "views/home/introduce/img/DF-26.png",
+                            "views/home/introduce/img/DF-33.png",
+                            "views/home/introduce/img/DF-34.png",
+                            "views/home/introduce/img/DF-35.png",
+                            "views/home/introduce/img/DF-36.png",
+                            "views/home/introduce/img/DF-37.png",
+                            "views/home/introduce/img/DF-38.png",
+                            "views/home/introduce/img/icon-26.png",
+                            "views/home/introduce/img/icon-27.png",
+                            "views/home/introduce/img/icon-28.png",
+                            "views/home/introduce/img/icon-29.png",
+                            "views/home/introduce/img/icon-30.png",
+                            "views/home/introduce/img/icon-31.png",
+                            "views/home/introduce/img/icon-32.png",
+                            "views/home/introduce/img/images_10.png",
+                            "views/home/introduce/img/images_16.png",
+                            "views/home/introduce/img/images_39.png",
+                            "views/home/introduce/img/images_41.png",
+                            "views/home/introduce/img/images_43.png",
+                            "views/home/introduce/img/images_45.png",
+                            "views/home/introduce/img/images_49.png",
+                            "views/home/introduce/img/images_67.png"
+
+                        );
                     }
 
                 })
