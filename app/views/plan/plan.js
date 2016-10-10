63a191695a460392ce898b0cb1633ac85bc6b8fe
jiasx <120084548@qq.com>
2016-10-10 15:55:50 +0800

    价格，购买套餐页面开发

--- app/views/plan/plan.js
+++ app/views/plan/plan.js
@@ -1,1 +1,21 @@
-/** * Created by sorcerer on 16/10/8. */angular.module('console.plan', [        {            files: [                'views/plan/plan.css'            ]        }    ])    .controller('planCtrl', ['$scope', function ($scope) {    }]);
+/**
+ * Created by sorcerer on 16/10/8.
+ */
+angular.module('console.plan', [
+        {
+            files: [
+                'views/plan/plan.css'
+            ]
+        }
+    ])
+    .controller('planCtrl', ['$scope','$rootScope','account', function ($scope,$rootScope,account) {
+        console.log('$rootScope.projects', $rootScope.projects);
+        account.get({n:'1'}, function (data) {
+            if (data.purchased) {
+                $('.plan_block_main').css("left","200px");
+            }else{
+                $('.plan_block_main').css("left","0");
+            }
+        })
+
+    }]);
