d0f05d203934be7759aee38fc699174baf856542
fooles <18730471450@163.com>
2016-10-21 10:44:18 +0800

    ms 切区 api

--- app/resource.js
+++ app/resource.js
@@ -562,14 +562,16 @@ define([
         }])
         
         .factory('modifySecret', ['$resource', 'GLOBAL', function ($resource, GLOBAL) {
-            var modifySecret = $resource(GLOBAL.host_k8s + '/namespaces/:namespace/secrets/:name', {
+            var modifySecret = $resource(GLOBAL.host_k8s + '/namespaces/:namespace/secrets/:name?region=:region', {
                 namespace: '@namespace',
-                name: '@name'
+                name: '@name',
+                region: '@region'
             }, {
                 update: {method: 'PUT'}
             })
             return modifySecret;
         }])
+
         .factory('deleteSecret', ['$resource', 'GLOBAL', function ($resource, GLOBAL) {
             var deleteSecret = $resource(GLOBAL.host_k8s + '/namespaces/:namespace/secrets/:name', {
                 namespace: '@namespace',
--- app/views/secret_detail/secret_detail.js
+++ app/views/secret_detail/secret_detail.js
@@ -222,7 +222,8 @@ angular.module('console.secret_detail', [
                 delete $scope.item.newarr;
                 modifySecret.update({
                     namespace: $rootScope.namespace,
-                    name: $stateParams.name
+                    name: $stateParams.name,
+                    region:$rootScope.region
                 }, $scope.item, function (res) {
                     console.log('test the item', res);
                     Toast.open('保存成功')
