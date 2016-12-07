8045e9ebf6b851cbe988d66bd5916a0a9ec68746
fooles <18730471450@163.com>
2016-12-07 19:22:42 +0800

    注册

--- app/views/regist/regist.js
+++ app/views/regist/regist.js
@@ -13,8 +13,8 @@ angular.module('home.regist', [])
                 }
                 //注册相关代码...
                 registration.regist({}, $scope.sendobj, function (data) {
-                    Alert.open('注册账号', '激活邮件发送成功!', '', true).then(function () {
-                        $state.go('home.index');
+                    Alert.open('注册账号', '注册成功,请登录', '', true).then(function () {
+                        $state.go('login');
                     })
                 }, function (err) {
                     console.log(err);
