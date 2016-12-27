9bbf8d9d594eeb0ada744b2396002c646948a3c3
jiasx <120084548@qq.com>
2016-12-27 14:38:22 +0800

    bug修改1

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -13,6 +13,18 @@ angular.module("console.sidebar", [
             templateUrl: 'components/sidebar/sidebar.html',
             controller: ['$state', '$scope', function($state, $scope){
                 $scope.$state = $state;
+               //alert($state.current.name)
+
+                //$scope.$watch('$state.current.name',function (n,o) {
+                //    //console.log('$state', n);
+                //    if (n === 'console.build_create_new' || n === 'console.build_detail'|| n==='console.build') {
+                //        $scope.build = true
+                //    }else {
+                //        $scope.build = false
+                //    }
+                //    //$scope.$apply()
+                //    //console.log('$scope.build', $scope.build);
+                //})
             }]
         }
     }]);
