ce4d065446ab3f4003737f0b9aeb86a4e0978af0
jingxy <953885034@qq.com>
2017-12-06 19:15:09 +0800

    侧边栏

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -11,17 +11,22 @@ angular.module("console.sidebar", [
             restrict: 'EA',
             replace: true,
             templateUrl: 'components/sidebar/sidebar.html',
-            controller: ['$state', '$scope', function($state, $scope){
+            controller: ['$state', '$scope','$rootScope', function($state, $scope,$rootScope){
                 $scope.goUrl = function(url){
                        if(url){
-                           $state.go(url)
+                           $state.go(url);
                        }else{
                            $(".zx_set_btn").removeClass("zx_set_btn_rotate");
                            $("#sidebar-container").removeClass("sider_zx");
                            $("#sidebar-right-fixed").removeClass("sidebar-fixed");
-                           $scope.dataForTheTree[3].children = [{name:'服务部署',url:'console.service',children:[]}];
+                           $rootScope.dataForTheTree[3].children = [{name:'服务部署',url:'console.service',children:[]}];
+                           if(!$(".zx_set_btn").hasClass('zx_set_btn_rotate')){
+                               $('.node-name').show()
+                               $('.tooltip-r').hide()
+                           }
                        }
                 }
+                $scope.activeStyle = false;
                 $scope.treeOptions = {
                     nodeChildren: "children",
                     dirSelectable: false,
@@ -36,13 +41,13 @@ angular.module("console.sidebar", [
                         labelSelected: "a8"
                     }
                 }
-                $scope.dataForTheTree =
+                $rootScope.dataForTheTree =
                     [
                         {name:'仪表盘',img:'icon25 icon25-dashboard',url:'console.dashboard',children:[]},
                         {name:'代码构建',img:'icon25 icon25-build',url:'console.build',children:[]},
                         {name:'镜像仓库',img:'icon25 icon25-repository',url:'console.image',children:[]},
                         {name:'服务部署',img:'icon25 icon25-deployment',url:'',children:[{name:'服务部署',url:'console.service',children:[]}]},
-                        {name:'资源管理',img:'icon25 icon25-resource',url:'console.resource_management',children:[]},
+                        {name:'资源管理',img:'icon25 icon25-resource',url:'console.resource_management',children:[]}
                     ];
 
                 var width = 0;
@@ -51,13 +56,19 @@ angular.module("console.sidebar", [
                     $(".zx_set_btn").toggleClass("zx_set_btn_rotate");
                     $("#sidebar-container").toggleClass("sider_zx");
                     $("#sidebar-right-fixed").toggleClass("sidebar-fixed");
+                    $(".sb-arrow").toggleClass("rotate");
                     if($(".zx_set_btn").hasClass('zx_set_btn_rotate')){
-                        $scope.dataForTheTree[3].children = [];
+                        $('.node-name').hide();
+                        $scope.activeStyle = true;
+                        $rootScope.dataForTheTree[3].children = [];
                     }else{
-                        $scope.dataForTheTree[3].children = [{name:'服务部署',url:'console.service',children:[]}];
+                        $('.node-name').show();
+                        $scope.activeStyle = false;
+                        $rootScope.dataForTheTree[3].children = [{name:'服务部署',url:'console.service',children:[]}];
 
                     }
                 }
+                $scope.showSelected
 
             }]
         }
