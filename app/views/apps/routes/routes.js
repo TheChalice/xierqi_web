5ba6fe71ffba682dc83e4a50c00feeb9a526814f
jingxy <953885034@qq.com>
2018-01-16 15:39:02 +0800

    route列表查询翻页

--- app/views/apps/routes/routes.js
+++ app/views/apps/routes/routes.js
@@ -5,29 +5,63 @@ angular.module('console.routes', [{
             'views/apps/apps.css'
         ]
     }])
-    .controller('RoutesCtrl', ['$scope', 'routes',
-        function($scope, routes) {
+    .controller('RoutesCtrl', ['$scope', 'routes','Route',
+        function($scope, routes,Route) {
             $scope.text = "No routes have been added to project " + $scope.namespace + ".";
-            if (routes) {
-                $scope.items = routes.items;
-            }
-            //分页
             $scope.grid = {
                 page: 1,
-                size: 2,
+                size: 10,
                 txt: ''
             };
-            $scope.$watch('grid.page', function(newVal, oldVal){
+            $scope.$watch('grid.page', function (newVal, oldVal) {
                 if (newVal != oldVal) {
                     refresh(newVal);
                 }
             });
-            var refresh = function(page) {
+            var refresh = function (page) {
                 $(document.body).animate({
                     scrollTop: 0
                 }, 200);
                 var skip = (page - 1) * $scope.grid.size;
-                $scope.items = $scope.data.slice(skip, skip + $scope.grid.size);
+                $scope.routeItem = $scope.items.slice(skip, skip + $scope.grid.size)||[];
             };
+            $scope.search = function (event) {
+                     $scope.grid.page = 1;
+                    if (!$scope.grid.txt) {
+                        $scope.items = angular.copy($scope.copyRoute);
+                        refresh(1);
+                        $scope.grid.total = $scope.items.length;
+                        return;
+                    }
+                    $scope.items = [];
+                    var iarr = [];
+                    var str = $scope.grid.txt;
+                    str = str.toLocaleLowerCase();
+                    angular.forEach($scope.copyRoute, function (item, i) {
+                        var nstr = item.metadata.name;
+                        nstr = nstr.toLocaleLowerCase();
+                        if (nstr.indexOf(str) !== -1) {
+                            iarr.push(item)
+                        }
+                    })
+                    if(iarr.length===0){
+                        $scope.text3='没有查询到相关数据';
+                    }
+                    else{
+                        $scope.text3='您还没有创建密钥';
+                    }
+                    $scope.items=angular.copy(iarr);
+                    refresh(1);
+                    $scope.grid.total = $scope.items.length;
+
+            };
+            if (routes) {
+                $scope.items = routes.items;
+                $scope.copyRoute = angular.copy($scope.items);
+                $scope.grid.total = $scope.items.length;
+                $scope.grid.page = 1;
+                $scope.grid.txt = '';
+                refresh(1);
+            }
         }
     ]);
