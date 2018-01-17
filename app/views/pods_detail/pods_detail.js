b4d5d5ff96ab09c562098df5ebd4311537148ef0
Your Name <you@example.com>
2018-01-17 15:29:45 +0800

    metrics部分代码提交

--- app/views/pods_detail/pods_detail.js
+++ app/views/pods_detail/pods_detail.js
@@ -8,292 +8,107 @@ angular.module('console.pods_detail', [
             ]
         }
     ])
-    .controller('podsdetailCtrl', ['$rootScope', '$scope', '$state', '$log', 'mypod','Ws',
-        function ($rootScope, $scope, $state, $log,mypod,Ws) {
+    .controller('podsdetailCtrl', ['$rootScope', '$scope', '$state', '$log', 'mypod','Ws','Metrics',
+        function ($rootScope, $scope, $state, $log,mypod,Ws,Metrics) {
             $scope.pod=angular.copy(mypod)
             $scope.environment = $scope.pod.spec.containers[0].env;
-
+            // $scope.uid = $scope.pod.metadata.uid;
+            // $scope.name = $scope.pod.metadata.name;
+            // console.log('uid',$scope.uid, $scope.name);
             $scope.$on('$destroy', function () {
                 Ws.clear();
             });
-          //memory模块设置
-          var times = (new Date()).getTime();
-          var setChart = function () {
-            return {
-                options: {
-                    chart: {
-                        type: 'spline'
 
-                    },
-                    title: {
-                        text: name,
-                        align: 'left',
-                        x: 0,
-                        style: {
-                            fontSize: '12px'
-                        }
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    tooltip: {
-                        backgroundColor: '#666',
-                        borderWidth: 0,
-                        shadow: false,
-                        style: {
-                            color: '#fff'
-                        }
-                    },
-                    legend: {
-                        enabled: false
-                    }
-                },
-                series: [{
-                    name: 'cpu',
-                    fillColor: {
-                        linearGradient: {x1: 0, y1: 1, x2: 0, y2: 0}, //横向渐变效果 如果将x2和y2值交换将会变成纵向渐变效果
-                        stops: [
-                            [0, Highcharts.Color('#fff').setOpacity(0.8).get('rgba')],
-                            [1, '#4ca7de']
-                        ]
-                    },
-                    lineColor: '#000', //线的颜色
-                    lineWidth: 0.5,  //线的粗细
-                    fillOpacity: 0.6,
-                    marker: {   //数据节点显示 
-                        enabled: true
-                    },
-                    yAxis: 1,
-                    data:[100,200,300,400],
-                    pointStart: times + 3600 * 1000,
-                    pointInterval: 15 * 60 * 1000 //时间间隔
-                }],
-                xAxis: {
-                    // categories: ['12:00','14:00', '16:00', '18:00', '20:00', '22:00', '24:00'],
-                    type: 'datetime',
-                    gridLineWidth: 1
-                },
-                yAxis: [{
-                    // gridLineDashStyle: 'ShortDash',
-                    title: {
-                        text: '',
-                    }
+             // newChart 曲线组件方法
+             var netChart = function (title,arr) {
+                return {
+                    options: {
+                        chart: {
+                            type: 'spline'
 
-                }, {
-                    // gridLineDashStyle: 'ShortDash',
-                    title: {
-                        text: 'CPU (%)',
-                        style: {
-                            color: '#f6a540'
-                        }
-                    },
-                    opposite: false
-                }],
-                size: {
-                    height: 230,
-                    width: 950
-                },
-                func: function (chart) {
-                    //setup some logic for the chart
-                }
-            };
-        };
-
-          $scope.chartmConfig = setChart();//memory模块设置
-
-
-
-           //CPU模块设置
-           var times = (new Date()).getTime();
-           var setChart = function () {
-             return {
-                 options: {
-                     chart: {
-                         type: 'spline'
- 
-                     },
-                     title: {
-                         text: name,
-                         align: 'left',
-                         x: 0,
-                         style: {
-                             fontSize: '12px'
-                         }
-                     },
-                     credits: {
-                         enabled: false
-                     },
-                     tooltip: {
-                         backgroundColor: '#666',
-                         borderWidth: 0,
-                         shadow: false,
-                         style: {
-                             color: '#fff'
-                         }
-                     },
-                     legend: {
-                         enabled: false
-                     }
-                 },
-                 series: [{
-                     name: 'cpu',
-                     fillColor: {
-                         linearGradient: {x1: 0, y1: 1, x2: 0, y2: 0}, //横向渐变效果 如果将x2和y2值交换将会变成纵向渐变效果
-                         stops: [
-                             [0, Highcharts.Color('#fff').setOpacity(0.8).get('rgba')],
-                             [1, '#4ca7de']
-                         ]
-                     },
-                     lineColor: '#000', //线的颜色
-                     lineWidth: 0.5,  //线的粗细
-                     fillOpacity: 0.6,
-                     marker: {   //数据节点显示 
-                         enabled: true
-                     },
-                     yAxis: 1,
-                     data:[100,200,300,400],
-                     pointStart: times + 3600 * 1000,
-                     pointInterval: 15 * 60 * 1000 //时间间隔
-                 }],
-                 xAxis: {
-                     // categories: ['12:00','14:00', '16:00', '18:00', '20:00', '22:00', '24:00'],
-                     type: 'datetime',
-                     gridLineWidth: 1
-                 },
-                 yAxis: [{
-                     // gridLineDashStyle: 'ShortDash',
-                     title: {
-                         text: '',
-                     }
- 
-                 }, {
-                     // gridLineDashStyle: 'ShortDash',
-                     title: {
-                         text: 'CPU (%)',
-                         style: {
-                             color: '#f6a540'
-                         }
-                     },
-                     opposite: false
-                 }],
-                 size: {
-                     height: 230,
-                     width: 950
-                 },
-                 func: function (chart) {
-                     //setup some logic for the chart
-                 }
-             };
-         };
- 
-           $scope.chartcConfig = setChart();//CPU模块设置
-
-
-           //Network模块设置
-           var times = (new Date()).getTime();
-           var setChart = function () {
-            return {
-                options: {
-                    chart: {
-                        type: 'spline'
-
-                    },
-                    title: {
-                        text: name,
-                        align: 'left',
-                        x: 0,
-                        style: {
-                            fontSize: '12px'
-                        }
-                    },
-                    credits: {
-                        enabled: false
-                    },
-                    tooltip: {
-                        backgroundColor: '#666',
-                        borderWidth: 0,
-                        shadow: false,
-                        style: {
-                            color: '#fff'
-                        }
-                    },
-                    legend: {
-                        enabled: true
-                    }
-                },
-                series: [{
-                    name: 'cpu',
-                    fillColor: {
-                        linearGradient: {x1: 0, y1: 1, x2: 0, y2: 0}, //横向渐变效果 如果将x2和y2值交换将会变成纵向渐变效果
-                        stops: [
-                            [0, Highcharts.Color('#fff').setOpacity(0.8).get('rgba')],
-                            [1, '#4ca7de']
-                        ]
-                    },
-                    lineColor: '#f00',
-                    fillOpacity: 0.6,
-                    marker: {
-                        enabled: true
-                    },
-                    yAxis: 1,
-                    data:[10,30,90,30],
-                    pointStart: times + 3600 * 1000,
-                    pointInterval: 15 * 60 * 1000 //时间间隔
-                },
-                    {
-                        name: '内存',
-                        fillColor: {
-                            linearGradient: {x1: 0, y1: 1, x2: 0, y2: 0}, //横向渐变效果 如果将x2和y2值交换将会变成纵向渐变效果
-                            stops: [
-                                [0, Highcharts.Color('#36a390').setOpacity(0.8).get('rgba')],
-                                [1, '#79d87e']
-                            ]
                         },
-                        lineColor: '#000',
-                        fillOpacity: 0.6,
-                        marker: {
+                        title: {
+                            text: name,
+                            align: 'left',
+                            x: 0,
+                            style: {
+                                fontSize: '12px'
+                            }
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        tooltip: {
+                            backgroundColor: '#666',
+                            borderWidth: 0,
+                            shadow: false,
+                            style: {
+                                color: '#fff'
+                            }
+                        },
+                        legend: {
                             enabled: true
-                        },
-                        yAxis: 0,
-                        data: [10,30,20,80],
-                        pointStart: times + 3600 * 1000,
-                        pointInterval: 15 * 60 * 1000 //时间间隔
-                    }],
-                xAxis: {
-                    // categories: ['12:00','14:00', '16:00', '18:00', '20:00', '22:00', '24:00'],
-                    type: 'datetime',
-                    gridLineWidth: 1
-                },
-                yAxis: [{
-                    // gridLineDashStyle: 'ShortDash',
-                    title: {
-                        text: 'memory (M)',
-                        style: {
-                            color: '#bec0c7'
-                        }
-                    }
-
-                }, {
-                    // gridLineDashStyle: 'ShortDash',
-                    title: {
-                        text: 'CPU (%)',
-                        style: {
-                            color: '#f6a540'
                         }
                     },
-                    opposite: true
-                }],
-                size: {
-                    height: 230,
-                    width: 950
-                },
-                func: function (chart) {
-                    //setup some logic for the chart
-                }
+                    series: arr,
+                    xAxis: {
+                        // categories: ['12:00','14:00', '16:00', '18:00', '20:00', '22:00', '24:00'],
+                        type: 'datetime',
+                        gridLineWidth: 1
+                    },
+                    yAxis: [{
+                        // gridLineDashStyle: 'ShortDash',
+                        title: {
+                            text: title,
+                            style: {
+                                color: '#bec0c7'
+                            }
+                        }
+
+                    }],
+                    size: {
+                        height: 230,
+                        width: 950
+                    },
+                    func: function (chart) {
+                        //setup some logic for the chart
+                    }
+                };
             };
-        };
- 
-           $scope.chartnConfig = setChart();//Network模块设置
+
+            var getOwnerReferences = function(apiObject) {
+                return _.get(apiObject, 'metadata.ownerReferences');
+            };
+            
+            //作用
+            var filterForController = function(apiObjects, controller) {
+                var controllerUID = _.get(controller, 'metadata.uid');
+                return _.filter(apiObjects, function(apiObject) {
+                    return _.some(getOwnerReferences(apiObject), {
+                        uid: controllerUID,
+                        controller: true
+                    });
+                });
+            }
+
+            
+
+        
+            var podschat = function(name,uid,type){
+                Metrics.mem.query({
+                    gauges: name+'/'+uid+'/'+type,
+                    bucketDuration: '120000ms',
+                    start:'1516103373792'
+                }, function (res) {
+                           console.log('-------------------------res',res) 
+                           $scope.CpuConfig = netChart('CPU/cores',res);
+                        
+                })        
+            }
+
+            podschat(mypod.spec.containers[0].name,mypod.metadata.uid,'network/rx_rate');
+            
+        
 
             
             
