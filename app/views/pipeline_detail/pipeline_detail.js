dfef42dba109847ec422753459be165ae5f57bd4
Your Name <you@example.com>
2018-03-12 20:08:20 +0800

    配置信息1

--- app/views/pipeline_detail/pipeline_detail.js
+++ app/views/pipeline_detail/pipeline_detail.js
@@ -8,9 +8,10 @@ angular.module('console.pipeline.detail', [
         ]
     }
 ])
-    .controller('pipelineDetailCtrl', ['$sce', 'ansi_ups', 'ImageStreamTag', 'deleteSecret', 'Ws', 'Sort', 'GLOBAL', '$rootScope', '$scope', '$log', '$state', '$stateParams', '$location', 'BuildConfig', 'Build', 'Confirm', 'UUID', 'WebhookLab', 'WebhookHub', 'WebhookLabDel', 'WebhookHubDel', 'ImageStream', 'WebhookLabget', 'WebhookGitget', 'toastr'
-        , function ($sce, ansi_ups, ImageStreamTag, deleteSecret, Ws, Sort, GLOBAL, $rootScope, $scope, $log, $state, $stateParams, $location, BuildConfig, Build, Confirm, UUID, WebhookLab, WebhookHub, WebhookLabDel, WebhookHubDel, ImageStream, WebhookLabget, WebhookGitget, toastr) {
-
+    .controller('pipelineDetailCtrl', [ '$rootScope', '$scope', '$log', '$state', '$stateParams', '$location', 'BuildConfig', 'Build', 'Confirm', 'toastr','BuildConfigs'
+        , function ($rootScope, $scope, $log, $state, $stateParams, $location, BuildConfig, Build, Confirm, toastr,BuildConfigs) {
+            $scope.BuildConfig = angular.copy(BuildConfigs);
+           
 
             $scope.getLog = function(idx){
                 
