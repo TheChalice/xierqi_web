2ea838cbe91f274e625f2b237f95b93483572298
fooles <18730471450@163.com>
2018-04-04 16:31:17 +0800

    icon

--- usr/local/bin/start.sh
+++ usr/local/bin/start.sh
@@ -14,6 +14,7 @@ sed -i 's/<api_server_addr>/'$API_SERVER_ADDR'/g' /usr/local/openresty/nginx/con
 sed -i 's/<api_proxy_addr>/'$API_PROXY_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_oauth_addr>/'$API_OAUTH_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_metrics_addr>/'$API_METRICS_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
+sed -i 's/<apis_prd_addr>/'$APIS_PRD_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_gitter_addr>/'$API_GITTER_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_volume_addr>/'$API_VOLUME_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_registry_addr>/'$API_REGISTRY_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
