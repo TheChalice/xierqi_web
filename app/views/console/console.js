66afe6ff3c7e351eca400ba6183e58b4345edc4d
jiangtong <jiangtong@asisinfo.com>
2018-07-07 16:00:27 +0800

    PRDBD_REQ_20180628_0064 构建镜像

--- app/views/console/console.js
+++ app/views/console/console.js
@@ -18,7 +18,36 @@ angular.module('console', [
             console.log('$location', $location);
 
             if (GLOBAL.sso_switch === 'true') {
+                Cookie.set('df_access_token', user.access_token + ',' + user.access_token, 23 * 3600 * 1000);
+                //Cookie.set('df_access_token', user.access_token + ',' + user.access_token, 23 * 3600 * 1000);
+                Cookie.set('region', 'cn-north-1', 24 * 3600 * 1000);
+                if ($stateParams.namespace) {
+                    if ($rootScope.user) {
+                        console.log('$rootScope.user', $rootScope.user.metadata.name);
+                    } else {
 
+                        $rootScope.user = {metadata:{
+                            name:$stateParams.namespace
+                        }};
+                    }
+                    var namespace = Cookie.get('namespace');
+                    var region = Cookie.get('region');
+                    if (region) {
+                        $rootScope.region = region;
+                    } else {
+                        console.log('noregion');
+                        $rootScope.region = 'cn-north-1';
+                        Cookie.set('region', $rootScope.region, 10 * 365 * 24 * 3600 * 1000);
+                    }
+
+                    if (namespace) {
+                        $rootScope.namespace = namespace;
+                    } else {
+                        //console.log('nonamespace');
+                        $rootScope.namespace = $rootScope.user.metadata.name;
+                        Cookie.set('namespace', $rootScope.namespace, 10 * 365 * 24 * 3600 * 1000);
+                    }
+                }
 
             }else {
                 if ($rootScope.user) {
