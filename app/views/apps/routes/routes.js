cf8fcf30d5ebd995907e386fa8b8b080d70e9f75
niusy <niusy@asiainfo.com>
2018-10-23 16:39:36 +0800

    容器应用 容器应用 部署镜像详情页

--- app/views/apps/routes/routes.js
+++ app/views/apps/routes/routes.js
@@ -1,13 +1,14 @@
 'use strict';
 angular.module('console.routes', [{
-        files: [
-            'components/searchbar/searchbar.js',
-            'views/apps/apps.css'
-        ]
-    }])
-    .controller('RoutesCtrl', ['$scope', 'routes','Route',
-        function($scope, routes,Route) {
-            $scope.text = "无";
+    files: [
+        'components/searchbar/searchbar.js',
+        'views/apps/apps.css'
+    ]
+}])
+    .controller('RoutesCtrl', ['$scope', 'routes', 'Route', 'Sort',
+        function ($scope, routes, Route, Sort) {
+            $scope.text = "您还没有域名管理";
+            $scope.begin_blank = true;
             //$scope.text = "No routes have been added to project " + $scope.namespace + ".";
             $scope.grid = {
                 page: 1,
@@ -24,34 +25,46 @@ angular.module('console.routes', [{
                     scrollTop: 0
                 }, 200);
                 var skip = (page - 1) * $scope.grid.size;
-                $scope.routeItem = $scope.items.slice(skip, skip + $scope.grid.size)||[];
+                $scope.routeItem = $scope.items.slice(skip, skip + $scope.grid.size) || [];
             };
             $scope.search = function (event) {
-                     $scope.grid.page = 1;
-                    if (!$scope.grid.txt) {
-                        $scope.items = angular.copy($scope.copyRoute);
-                        refresh(1);
-                        $scope.grid.total = $scope.items.length;
-                        return;
-                    }
-                    $scope.items = [];
-                    var iarr = [];
-                    var str = $scope.grid.txt;
-                    str = str.toLocaleLowerCase();
-                    angular.forEach($scope.copyRoute, function (item, i) {
-                        var nstr = item.metadata.name;
-                        nstr = nstr.toLocaleLowerCase();
-                        if (nstr.indexOf(str) !== -1) {
-                            iarr.push(item)
-                        }
-                    })
-                    $scope.items=angular.copy(iarr);
+                $scope.grid.page = 1;
+                if (!$scope.grid.txt) {
+                    $scope.items = angular.copy($scope.copyRoute);
                     refresh(1);
                     $scope.grid.total = $scope.items.length;
+                    return;
+                }
+                $scope.items = [];
+                var iarr = [];
+                var str = $scope.grid.txt;
+                str = str.toLocaleLowerCase();
+                angular.forEach($scope.copyRoute, function (item, i) {
+                    var nstr = item.metadata.name;
+                    nstr = nstr.toLocaleLowerCase();
+                    if (nstr.indexOf(str) !== -1) {
+                        iarr.push(item)
+                    }
+                })
+
+                $scope.isQuery = false;
+                if (iarr.length === 0) {
+                    $scope.isQuery = true;
+                    $scope.begin_blank = false;
+                    $scope.text = '没有查询到符合条件的数据';
+                    // console.log($scope.items.length);
+                }
+                else {
+                    $scope.text = '您还没有任何域名管理数据，现在就创建一个吧';
+                }
+                $scope.items = angular.copy(iarr);
+                refresh(1);
+                $scope.grid.total = $scope.items.length;
 
             };
             if (routes) {
                 $scope.items = routes.items;
+                $scope.items = Sort.sort(routes.items, -1)
                 $scope.copyRoute = angular.copy($scope.items);
                 $scope.grid.total = $scope.items.length;
                 $scope.grid.page = 1;
--- app/views/apps/stateful-sets/stateful-sets.js
+++ app/views/apps/stateful-sets/stateful-sets.js
@@ -1,16 +1,17 @@
 'use strict';
 angular.module('console.stateful-sets', [{
-        files: [
-            'components/searchbar/searchbar.js',
-            'views/apps/apps.css'
-        ]
-    }])
-    .controller('Stateful-setsCtrl', ['statefulsets', '$scope','Ws','$rootScope','$log',
-        function(statefulsets, $scope,Ws,$rootScope,$log) {
-            $scope.text = "无";
-            var getStatefulsets = function(){
-                statefulsets.get({ namespace: $scope.namespace }, function(res) {
+    files: [
+        'components/searchbar/searchbar.js',
+        'views/apps/apps.css'
+    ]
+}])
+    .controller('Stateful-setsCtrl', ['statefulsets', '$scope', 'Ws', '$rootScope', '$log', 'Sort',
+        function (statefulsets, $scope, Ws, $rootScope, $log, Sort) {
+            $scope.text = "当前列表暂时没有数据";
+            var getStatefulsets = function () {
+                statefulsets.get({namespace: $scope.namespace}, function (res) {
                     $scope.items = res.items;
+                    $scope.items = Sort.sort($scope.items, -1);
                     $scope.copySets = angular.copy($scope.items);
                     $scope.grid.total = $scope.items.length;
                     $scope.grid.page = 1;
@@ -19,7 +20,7 @@ angular.module('console.stateful-sets', [{
                     watchPc(res.metadata.resourceVersion);
                     refresh(1);
                 });
-            }
+            };
             getStatefulsets();
             $scope.grid = {
                 page: 1,
@@ -59,7 +60,7 @@ angular.module('console.stateful-sets', [{
                 $scope.resourceVersion = data.object.metadata.resourceVersion;
                 //data.object.spec.resources.requests.storage=data.object.spec.resources.requests.storage.replace('i','B')
                 if (data.type == 'ADDED') {
-                    $scope.items.unshift(data.object)
+                    $scope.items.unshift(data.object);
                     refresh(1);
                     $scope.$apply();
                 } else if (data.type == "MODIFIED") {
@@ -71,17 +72,17 @@ angular.module('console.stateful-sets', [{
                             $scope.$apply();
                         }
                     })
-                }else if (data.type == "DELETED") {
+                } else if (data.type == "DELETED") {
                     //data.object.spec.resources.requests.storage=data.object.spec.resources.requests.storage.replace('i','B')
                     angular.forEach($scope.items, function (item, i) {
                         if (item.metadata.name == data.object.metadata.name) {
-                            $scope.items.splice(i,1)
+                            $scope.items.splice(i, 1);
                             refresh(1);
                             $scope.$apply();
                         }
                     })
                 }
-            }
+            };
             $scope.$watch('grid.page', function (newVal, oldVal) {
                 if (newVal != oldVal) {
                     refresh(newVal);
@@ -92,7 +93,7 @@ angular.module('console.stateful-sets', [{
                     scrollTop: 0
                 }, 200);
                 var skip = (page - 1) * $scope.grid.size;
-                $scope.setsItem = $scope.items.slice(skip, skip + $scope.grid.size)||[];
+                $scope.setsItem = $scope.items.slice(skip, skip + $scope.grid.size) || [];
             };
             $scope.search = function (event) {
                 $scope.grid.page = 1;
@@ -112,14 +113,17 @@ angular.module('console.stateful-sets', [{
                     if (nstr.indexOf(str) !== -1) {
                         iarr.push(item)
                     }
-                })
-                if(iarr.length===0){
-                    $scope.text3='没有查询到相关数据';
+                });
+                $scope.isQuery = false;
+                if (iarr.length === 0) {
+                    $scope.isQuery = true;
+                    $scope.text = '没有查询到符合条件的数据';
+                    // console.log($scope.items.length);
                 }
-                else{
-                    $scope.text3='您还没有创建密钥卷';
+                else {
+                    $scope.text = '您还没有任何创建密钥卷数据，现在就创建一个吧';
                 }
-                $scope.items=angular.copy(iarr);
+                $scope.items = angular.copy(iarr);
                 refresh(1);
                 $scope.grid.total = $scope.items.length;
 
