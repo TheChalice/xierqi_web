35875c0ef354351faa0abeb0c372c95b3fbff305
niusy <niusy@asiainfo.com>
2018-11-06 17:27:29 +0800

    监视样式更改

--- app/views/monitoring/monitoring.js
+++ app/views/monitoring/monitoring.js
@@ -11,30 +11,29 @@ angular.module('console.monitoring', [
         ]
     }
 ])
-    .controller('MonitoringCtrl', ['$rootScope', '$scope','$state', 'monitoringPods', 'monitoringReplicas', 'monitoringReplicaSet', 'BuildConfig', 'Build', 'Sort','monitoringStatefulSets','monitoringBuild',
-        function ($rootScope, $scope, $state,monitoringPods, monitoringReplicas, monitoringReplicaSet, BuildConfig, Build, Sort,monitoringStatefulSets,monitoringBuild) {
+    .controller('MonitoringCtrl', ['$rootScope', '$scope', '$state', 'monitoringPods', 'monitoringReplicas', 'monitoringReplicaSet', 'BuildConfig', 'Build', 'Sort', 'monitoringStatefulSets', 'monitoringBuild',
+        function ($rootScope, $scope, $state, monitoringPods, monitoringReplicas, monitoringReplicaSet, BuildConfig, Build, Sort, monitoringStatefulSets, monitoringBuild) {
             $scope.bodyclass = true;
             $scope.allList = ['All', 'Pods', 'Deployments', 'Builds', 'Stateful Sets'];
             $scope.curListName = "All";
             $scope.podsItem = angular.copy(monitoringPods);
             $scope.replicasItem = angular.copy(monitoringReplicas);
             $scope.replicaSetItem = angular.copy(monitoringReplicaSet);
-            $scope.statefulSets =  angular.copy(monitoringStatefulSets);
-            $scope.builds= angular.copy(monitoringBuild);
-            /////////
-             $scope.editEvent = function () {
-                 $state.go("console.events", { namespace: $rootScope.namespace })
-             };
+            $scope.statefulSets = angular.copy(monitoringStatefulSets);
+            $scope.builds = angular.copy(monitoringBuild);
 
+            $scope.editEvent = function () {
+                $state.go("console.events", {namespace: $rootScope.namespace})
+            };
             $scope.savelog = function (log) {
                 var filename = _.get($scope, 'object.metadata.name', 'openshift') + '.log';
-                var blob = new Blob([$scope.result], { type: "text/plain;charset=utf-8" });
+                var blob = new Blob([$scope.result], {type: "text/plain;charset=utf-8"});
                 saveAs(blob, filename);
             };
-            $scope.checkCurListName = function(name){
-                console.log('name',name);
+            $scope.checkCurListName = function (name) {
+                console.log('name', name);
                 $scope.curListName = name;
-                console.log('$scope.curListName',$scope.curListName);
+                console.log('$scope.curListName', $scope.curListName);
             };
             ////rc监控
             var getOwnerReferences = function (apiObject) {
@@ -51,41 +50,41 @@ angular.module('console.monitoring', [
                 });
             };
             ////////列表展开收缩
-            $scope.openCon = function(idx,type){
-                if(type=='pod'){
-                    if($scope.podsItem.items[idx].isLog){
+            $scope.openCon = function (idx, type) {
+                if (type == 'pod') {
+                    if ($scope.podsItem.items[idx].isLog) {
                         $scope.podsItem.items[idx].isLog = false;
-                    }else{
+                    } else {
                         $scope.podsItem.items[idx].isLog = true;
                     }
-                }else if(type=='rc'){
-                    if($scope.replicasItem.items[idx].isLog){
+                } else if (type == 'rc') {
+                    if ($scope.replicasItem.items[idx].isLog) {
                         $scope.replicasItem.items[idx].isLog = false;
-                    }else{
+                    } else {
                         $scope.replicasItem.items[idx].isLog = true;
                     }
-                    $scope.rcPods = filterForController($scope.podsItem.items,$scope.replicasItem.items[idx]);
+                    $scope.rcPods = filterForController($scope.podsItem.items, $scope.replicasItem.items[idx]);
                     var poduid = [];
                     for (var i = 0; i < $scope.rcPods.length; i++) {
                         poduid.push($scope.rcPods[i].metadata.uid);
                     }
                     $scope.rcPoduid = poduid.join('|');
-                }else if(type=='stateful'){
-                    if($scope.statefulSets.items[idx].isLog){
+                } else if (type == 'stateful') {
+                    if ($scope.statefulSets.items[idx].isLog) {
                         $scope.statefulSets.items[idx].isLog = false;
-                    }else{
+                    } else {
                         $scope.statefulSets.items[idx].isLog = true;
                     }
-                    $scope.statefulPods = filterForController($scope.podsItem.items,$scope.statefulSets.items[idx]);
+                    $scope.statefulPods = filterForController($scope.podsItem.items, $scope.statefulSets.items[idx]);
                     var poduid = [];
                     for (var i = 0; i < $scope.statefulPods.length; i++) {
                         poduid.push($scope.statefulPods[i].metadata.uid);
                     }
                     $scope.statefulPoduid = poduid.join('|');
-                }else if(type=='bc'){
-                    if($scope.builds.items[idx].isLog){
+                } else if (type == 'bc') {
+                    if ($scope.builds.items[idx].isLog) {
                         $scope.builds.items[idx].isLog = false;
-                    }else{
+                    } else {
                         $scope.builds.items[idx].isLog = true;
                     }
                 }
