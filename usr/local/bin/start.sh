dc9d00c6ec589b632af510709e45138902b822f1
jiangtong <jiangtong@asisinfo.com>
2018-11-06 16:00:13 +0800

    Merge branches 'develop' and 'jtp0feaure' of http://10.1.236.106/BDT_FDC/web-console into jtp0feaure
    # Conflicts:
    #	app/views/admin/admin.html
    #	app/views/image_detail/image_detail.html
    #	app/views/primage_detail/primage_detail.html

--- usr/local/bin/start.sh
+++ usr/local/bin/start.sh
@@ -5,6 +5,14 @@
 # INTERNAL_REGISTRY_ADDR internal registry, like docker-registry.default.svc:5000
 
 sed -i 's/<ROUTER_DOMAIN_SUFFIX>/'$ROUTER_DOMAIN_SUFFIX'/g'  /datafoundry/dist/app.js
+
+sed -i 's~<OPERATION_CONTROL_URL>~'$OPERATION_CONTROL_URL'~g'  /datafoundry/dist/app.js
+sed -i 's~<DATA_ASSETS_URL>~'$DATA_ASSETS_URL'~g'  /datafoundry/dist/app.js
+sed -i 's~<OPERATION_CENTER_URL>~'$OPERATION_CENTER_URL'~g'  /datafoundry/dist/app.js
+sed -i 's~<APPLICATION_MANAGEMENT_URL>~'$APPLICATION_MANAGEMENT_URL'~g'  /datafoundry/dist/app.js
+sed -i 's~<USER_CENTER_URL>~'$USER_CENTER_URL'~g'  /datafoundry/dist/app.js
+sed -i 's~<SMALL_BELL_URL>~'$SMALL_BELL_URL'~g'  /datafoundry/dist/app.js
+
 sed -i 's/<REGISTRY_PUBLIC_ADDR>/'$REGISTRY_PUBLIC_ADDR'/g'  /datafoundry/dist/app.js
 sed -i 's/<REGISTRY_PRIVATE_ADDR>/'$REGISTRY_PRIVATE_ADDR'/g'  /datafoundry/dist/app.js
 sed -i 's/<INTERNAL_REGISTRY_ADDR>/'$INTERNAL_REGISTRY_ADDR'/g'  /datafoundry/dist/app.js
@@ -19,8 +27,12 @@ sed -i 's/<redis_host>/'$REDIS_HOST'/g' /usr/local/openresty/nginx/conf/nginx.co
 sed -i 's/<redis_port>/'$REDIS_PORT'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<redis_password>/'$REDIS_PASSWORD'/g' /usr/local/openresty/nginx/conf/nginx.conf
 
+sed -i 's/<api_aipaas_addr>/'$API_AIPAAS_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
+
+
 sed -i 's/<api_server_addr>/'$API_SERVER_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_proxy_addr>/'$API_PROXY_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
+sed -i 's/<api_uploadimage_addr>/'$API_UPLOADIMAGE_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 # sed -i 's/<api_oauth_addr>/'$API_OAUTH_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<api_metrics_addr>/'$API_METRICS_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
 sed -i 's/<apis_prd_addr>/'$APIS_PRD_ADDR'/g' /usr/local/openresty/nginx/conf/nginx.conf
