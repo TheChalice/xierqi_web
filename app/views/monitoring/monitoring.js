82d7f1529382bcbfca23b7ce1335726b7237bf8e
niusy <niusy@asiainfo.com>
2018-11-07 15:03:14 +0800

    monitoring of 'filter by name'

--- app/views/monitoring/monitoring.js
+++ app/views/monitoring/monitoring.js
@@ -7,7 +7,7 @@ angular.module('console.monitoring', [
     {
         files: [
             'views/monitoring/monitoring.css',
-            'components/public_metrics/public_metrics.js',
+            'components/public_metrics/public_metrics.js'
         ]
     }
 ])
@@ -17,11 +17,27 @@ angular.module('console.monitoring', [
             $scope.allList = ['All', 'Pods', 'Deployments', 'Builds', 'Stateful Sets'];
             $scope.curListName = "All";
             $scope.podsItem = angular.copy(monitoringPods);
+            $scope.podsItemData = $scope.podsItem.items;
             $scope.replicasItem = angular.copy(monitoringReplicas);
+            $scope.replicasItemData = $scope.replicasItem.items;
             $scope.replicaSetItem = angular.copy(monitoringReplicaSet);
             $scope.statefulSets = angular.copy(monitoringStatefulSets);
+            $scope.statefulSetsData = $scope.statefulSets.items;
             $scope.builds = angular.copy(monitoringBuild);
-
+            $scope.buildsData = $scope.builds.items;
+            $scope.grid = {
+                txt : ''
+            };
+            var refresh = function () {
+                console.log('refresh');
+                $(document.body).animate({
+                    scrollTop: 0
+                }, 200);
+                $scope.podsItemData = $scope.podsItem.items;
+                $scope.replicasItemData = $scope.replicasItem.items;
+                $scope.statefulSetsData = $scope.statefulSets.items;
+                $scope.buildsData = $scope.builds.items;
+            };
             $scope.editEvent = function () {
                 $state.go("console.events", {namespace: $rootScope.namespace})
             };
@@ -33,7 +49,53 @@ angular.module('console.monitoring', [
             $scope.checkCurListName = function (name) {
                 console.log('name', name);
                 $scope.curListName = name;
-                console.log('$scope.curListName', $scope.curListName);
+                // console.log('$scope.curListName', $scope.curListName);
+            };
+
+            $scope.searchName = function (name) {
+                console.log('name', name);
+                if(!$scope.grid.txt){
+                    refresh();
+                    return
+                }else{
+                    var arr = [];
+                    var arrReplicasItem = [];
+                    var arrStatefulSets = [];
+                    var arrBuilds = [];
+                    var str = $scope.grid.txt;
+                    //in pods
+                    for (var i = 0; i < $scope.podsItem.items.length; i++) {
+                        var nstr = $scope.podsItem.items[i].metadata.name;
+                        if (nstr.indexOf(str) !== -1) {
+                            arr.push($scope.podsItem.items[i])
+                        }
+                    }
+                    $scope.podsItemData = arr;
+                    //in dc
+                    for (var j = 0; j < $scope.replicasItem.items.length; j++) {
+                        var nstrArrReplicasItem = $scope.replicasItem.items[j].metadata.name;
+                        if (nstrArrReplicasItem.indexOf(str) !== -1) {
+                            arrReplicasItem.push($scope.replicasItem.items[j])
+                        }
+                    }
+                    $scope.replicasItemData = arrReplicasItem;
+                    //in statefulSets
+                    for (var l = 0; l < $scope.statefulSets.items.length; l++) {
+                        var nstrStatefulSets = $scope.statefulSets.items[l].metadata.name;
+                        if (nstrStatefulSets.indexOf(str) !== -1) {
+                            arrStatefulSets.push($scope.statefulSets.items[l])
+                        }
+                    }
+                    $scope.statefulSetsData = arrStatefulSets;
+                    // in builds
+                    for (var k = 0; k < $scope.builds.items.length; k++) {
+                        var nstrBuilds = $scope.builds.items[k].metadata.name;
+                        if (nstrBuilds.indexOf(str) !== -1) {
+                            arrBuilds.push($scope.builds.items[k])
+                        }
+                    }
+                    $scope.buildsData = arrBuilds;
+                }
             };
             ////rc监控
             var getOwnerReferences = function (apiObject) {
@@ -89,7 +151,7 @@ angular.module('console.monitoring', [
                     }
                 }
 
-            }
+            };
             $scope.kinds = [
                 {
                     kind: "All"
