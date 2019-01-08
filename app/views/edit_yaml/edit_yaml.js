08b7142911ad0794e1fcca8fb0c1504569898122
niusy <niusy@asiainfo.com>
2019-01-08 10:17:36 +0800

    编辑yaml

--- app/views/edit_yaml/edit_yaml.js
+++ app/views/edit_yaml/edit_yaml.js
@@ -6,8 +6,7 @@ angular.module('console.edit_yaml_file', [
     }])
     .controller('EditYamlCtrl', ['$location', '$stateParams', 'toastr', '$rootScope', '$scope', '$state', 'EditYamlDeployment', 'EditYamlDeploymentConfigs',
         function ($location, $stateParams, toastr, $rootScope, $scope, $state, EditYamlDeployment, EditYamlDeploymentConfigs) {
-            console.log('EditYamlCtrl---type', $stateParams.kind);
-            // var findType = $stateParams.kind;
+            // console.log('EditYamlCtrl---type', $stateParams);
             $scope.textError = false;
             function setData(data) {
                 if (!data) return;
@@ -16,28 +15,22 @@ angular.module('console.edit_yaml_file', [
                 $scope.original = data;
                 $scope.newData = {};
                 $scope.newData.resource = angular.copy(data);
-                // console.log('$scope.newData.resource', $scope.newData.resource);
             }
             function saveUpdateData(dataResource,kind) {
-                if ($scope.newData.resource.kind !== $scope.original.kind){
+                if ($scope.newData.resource.kind !== $scope.original.kind
+                    || $scope.newData.resource.apiVersion !== $scope.original.apiVersion
+                    || $scope.newData.resource.apiVersion !== $scope.original.apiVersion
+                    || $scope.newData.resource.metadata.name !== $scope.original.metadata.name
+                    || $scope.newData.resource.metadata.namespace !== $scope.original.metadata.namespace){
                     $scope.textError = true;
-                    $scope.textErrorMessage = 'kind 不能修改'
-                }else if ($scope.newData.resource.apiVersion !== $scope.original.apiVersion){
-                    $scope.textError = true;
-                    $scope.textErrorMessage = 'apiVersion 不能修改'
-                }else if ($scope.newData.resource.metadata.name !== $scope.original.metadata.name){
-                    $scope.textError = true;
-                    $scope.textErrorMessage = 'name 不能修改'
-                }else if ($scope.newData.resource.metadata.namespace !== $scope.original.metadata.namespace){
-                    $scope.textError = true;
-                    $scope.textErrorMessage = 'namespace 不能修改'
+                    $scope.textErrorMessage = '不能修改'
                 }else {
                     dataResource.put({
-                        namespace: $stateParams.namespace,
+                        namespace: $rootScope.namespace,
                         name: $stateParams.name
                         // kind: $stateParams.kind
                     }, $scope.newData.resource, function (response) {
-                        console.log('resres', response);
+                        // console.log('resres', response);
                         toastr.success('编辑成功', {
                             timeOut: 2000,
                             closeButton: true
@@ -45,12 +38,12 @@ angular.module('console.edit_yaml_file', [
                         $scope.resource = response;
                         if(kind === 'Deployment'){
                             $state.go('console.deployment_detail', {
-                                namespace: $stateParams.namespace,
+                                namespace: $rootScope.namespace,
                                 name: $stateParams.name
                             });
                         }else if (kind === 'DeploymentConfig'){
                             $state.go('console.deploymentconfig_detail', {
-                                namespace: $stateParams.namespace,
+                                namespace: $rootScope.namespace,
                                 name: $stateParams.name
                             });
                         }
@@ -67,12 +60,12 @@ angular.module('console.edit_yaml_file', [
             function cancelUpdateData(kind) {
                 if(kind === 'Deployment'){
                     $state.go('console.deployment_detail', {
-                        namespace: $stateParams.namespace,
+                        namespace: $rootScope.namespace,
                         name: $stateParams.name
                     });
                 }else if (kind === 'DeploymentConfig'){
                     $state.go('console.deploymentconfig_detail', {
-                        namespace: $stateParams.namespace,
+                        namespace: $rootScope.namespace,
                         name: $stateParams.name
                     });
                 }
