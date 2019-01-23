6f1055c674f5f1dd4d20e8b0dfca213ba438e4a5
jingxy3 <953885034@qq.com>
2019-01-23 16:35:08 +0800

    PRDBD_REQ_20180628_0063/概览页面添加限额查看1

--- app/views/overview/overview.js
+++ app/views/overview/overview.js
@@ -11,7 +11,8 @@ angular.module('overview', [
     .controller('overviewCtrl', ['GLOBAL','$scope', '$rootScope', 'Cookie', 'user', 'dclist', 'ServiceTenant', 'ToolTenant', 'AcessInformation','Sort','quotaInfo',
         function (GLOBAL,$scope, $rootScope, Cookie, user, dclist, ServiceTenant, ToolTenant, AcessInformation,Sort,quotaInfo) {
             $scope.quotaInfo = function(name,quota){
-                quotaInfo.open(name,quota);
+
+                quotaInfo.open(name,JSON.parse(quota));
             }
             if ($rootScope.user) {
                 console.log('$rootScope.user', $rootScope.user.metadata.name);
