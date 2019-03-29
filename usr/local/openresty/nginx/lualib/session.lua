44f83ef66ad74685843a5c2135f7fe8e23182223
jiangtong <jiangtong@asisinfo.com>
2019-03-29 15:47:38 +0800

    test

--- usr/local/openresty/nginx/lualib/session.lua
+++ usr/local/openresty/nginx/lualib/session.lua
@@ -3,30 +3,22 @@ package.path = package.path .. ";/usr/local/openresty/nginx/lualib/?.lua"
 
 local authorize = require "comm.authorize"
 local json = require "cjson"
-local api_server = os.getenv("API_SERVER_ADDR")
-
-local redis_host = os.getenv("REDIS_HOST")
-local redis_port = strutil.atoi(os.getenv("REDIS_PORT"))
-local redis_password = os.getenv("REDIS_PASSWORD")
-
-
-
 
 local function sessionToken(username)
     local tokentool = authorize.new()
-    local tokencache = tokentool:has_token(username,redis_host,redis_port,redis_password)
+    local tokencache = tokentool:has_token(username)
 
     if tokencache == ngx.null then
 
         local token = {}
 
-        token = tokentool:auth(username,api_server)
+        token = tokentool:auth(username)
         if token == ngx.null then
             ngx.log(ngx.ERR, "can not authorize by openshift.")
             ngx.status = 401
             return ngx.exit(401)
         end
-        tokentool:add_bearer_token_ttl(username, token.expires_in, tokentool:auth_str(token.token_type, token.access_token),redis_host,redis_port,redis_password)
+        tokentool:add_bearer_token_ttl(username, token.expires_in, tokentool:auth_str(token.token_type, token.access_token))
         local tokenjson = json.encode{ access_token = token.access_token }
         ngx.status = 200
         ngx.header["access_token"] = token.access_token
@@ -42,16 +34,18 @@ local function sessionToken(username)
 end
 
 
-
+-- local request_uri = ngx.var.request_uri
+-- ngx.say(request_uri)
 
 local cas_loginname = ngx.req.get_headers()["X-Forwarded-User"]
 
 if not cas_loginname then
-
+    -- ngx.status = 401
+    -- ngx.say("unauthorized")
+    -- ngx.exit(401)
     ngx.log(ngx.ERR, "header 'X-Forwarded-User' not found.")
     nginx.status = 401
     return nginx.exit(401)
 else
     sessionToken(cas_loginname)
 end
-
